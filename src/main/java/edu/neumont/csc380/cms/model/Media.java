008bb5e2a62e60b372ff9e75074897c8530f3916
Greg Probst <gprobst@student.neumont.edu>
2014-04-24 10:17:37 -0600

    Media Object

--- src/main/java/edu/neumont/csc380/cms/model/Media.java
+++ src/main/java/edu/neumont/csc380/cms/model/Media.java
@@ -1,5 +1,90 @@
 package edu.neumont.csc380.cms.model;
 
+import java.awt.Dimension;
+import java.util.Date;
+
 public class Media {
+	private long id;
+	private long ownerId;
+	private MediaType type;
+	private String imgaeLocation;
+	private String thumbnailLocation;
+	private String caption;
+	private Dimension dimensions;
+	private Date dateCreated;
 	
+	public Media( long id, long ownerId, MediaType type, String imageLocation, String thumbnailLocation, String caption, Dimension dimensions ){
+		this.id = id;
+		this.ownerId = ownerId;
+		this.type = type;
+		this.imgaeLocation = imageLocation;
+		this.thumbnailLocation = thumbnailLocation;
+		this.caption = caption;
+		this.dimensions = dimensions;
+		this.dateCreated = new Date();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(long ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public MediaType getType() {
+		return type;
+	}
+
+	public void setType(MediaType type) {
+		this.type = type;
+	}
+
+	public String getImgaeLocation() {
+		return imgaeLocation;
+	}
+
+	public void setImgaeLocation(String imgaeLocation) {
+		this.imgaeLocation = imgaeLocation;
+	}
+
+	public String getThumbnailLocation() {
+		return thumbnailLocation;
+	}
+
+	public void setThumbnailLocation(String thumbnailLocation) {
+		this.thumbnailLocation = thumbnailLocation;
+	}
+
+	public String getCaption() {
+		return caption;
+	}
+
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	public Dimension getDimensions() {
+		return dimensions;
+	}
+
+	public void setDimensions(Dimension dimensions) {
+		this.dimensions = dimensions;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
 }
