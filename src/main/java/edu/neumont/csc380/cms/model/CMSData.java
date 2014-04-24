6c3c29a05c285f1181e8d0bf8b9309b64f31ba58
Felix Milea-Ciobanu <felixmilea@gmail.com>
2014-04-24 10:12:28 -0600

    slight changes to CMS data

--- src/main/java/edu/neumont/csc380/cms/model/CMSData.java
+++ src/main/java/edu/neumont/csc380/cms/model/CMSData.java
@@ -1,19 +1,24 @@
 package edu.neumont.csc380.cms.model;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CMSData {
-	private static HashMap<Long, Media> mediaMap;
+	private static Map<Long, Media> mediaMap;
 	private static CMSData data;
-	
-	public static CMSData getInstance(){
-		if(data == null){
+
+	public static CMSData getInstance() {
+		if (data == null) {
 			data = new CMSData();
 		}
 		return data;
 	}
-	
-	private CMSData(){
-		
+
+	public Media getMedia(Long id) {
+		return mediaMap.get(id);
+	}
+
+	private CMSData() {
+		mediaMap = new HashMap<Long, Media>();
 	}
 }
