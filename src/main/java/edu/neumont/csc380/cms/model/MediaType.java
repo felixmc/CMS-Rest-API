6b781e9e3bccc8f4e1808595fef7729b87865d1d
Felix Milea-Ciobanu <felixmilea@gmail.com>
2014-04-28 09:44:35 -0600

    fixed media type encapsulation

--- src/main/java/edu/neumont/csc380/cms/model/MediaType.java
+++ src/main/java/edu/neumont/csc380/cms/model/MediaType.java
@@ -2,10 +2,14 @@ package edu.neumont.csc380.cms.model;
 
 public enum MediaType {
 	JPEG(false), PNG(false), MOV(true), MP4(true);
-	
-	public boolean isVideo;
-	
-	MediaType(boolean isVideo){
+
+	private boolean isVideo;
+
+	MediaType(boolean isVideo) {
 		this.isVideo = isVideo;
+	}
+
+	public boolean isVideo() {
+		return isVideo;
 	}
 }
