f8e8ca7b8e2058dc9f43f37a51cecf9b919122cf
Greg Probst <gprobst@student.neumont.edu>
2014-04-29 17:39:01 -0600

    data stuff and pics

--- src/main/java/edu/neumont/csc380/cms/model/CMSData.java
+++ src/main/java/edu/neumont/csc380/cms/model/CMSData.java
@@ -1,12 +1,20 @@
 package edu.neumont.csc380.cms.model;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
 
 public class CMSData {
 	private static Map<Long, Media> mediaMap;
 	private static Map<Long, User> userMap;
 	private static Map<Long, Auction> auctionMap;
+	private static Map<Long, byte[]> dataMap;
 	private static long currentID;
 
 	private static CMSData data;
@@ -18,13 +26,18 @@ public class CMSData {
 		return data;
 	}
 
-	public void addMedia(Media mediaObject) {
+	public void addMedia(Media mediaObject, byte[] data) {
 		mediaObject.setId(getId());
 		mediaMap.put(mediaObject.getId(), mediaObject);
+		dataMap.put(mediaObject.getId(), data);
 	}
 
 	public Media getMedia(Long id) {
 		return mediaMap.get(id);
+	}
+	
+	public byte[] getData(long id){
+		return dataMap.get(id);
 	}
 
 	public Media removeMedia(Long id) {
@@ -36,24 +49,42 @@ public class CMSData {
 		mediaMap = new HashMap<Long, Media>();
 		userMap = new HashMap<Long, User>();
 		auctionMap = new HashMap<Long, Auction>();
+		dataMap = new HashMap<Long, byte[]>();
 
 		User user = new User();
 		user.setUserId(getId());
 		userMap.put(user.getUserId(), user);
 
+		BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File("duck.jpg"));
+		} catch (IOException e) {
+		}
+		
+		WritableRaster raster = img.getRaster();
+		DataBufferByte data   = (DataBufferByte) raster.getDataBuffer();
+
 		Media userMedia = new Media();
-		addMedia(userMedia);
+		addMedia(userMedia, data.getData());
 		userMedia.setCaption("My profile pic! lolz");
 		userMedia.setOwnerId(user.getUserId());
 		userMedia.setMediaLocation("/media/" + userMedia.getId() + "/"
 				+ "/data");
 		userMedia.setThumbnailLocation("/media/" + userMedia.getId() + "/"
 				+ "/thumb");
-		userMedia.setType(MediaType.PNG);
+		userMedia.setType(MediaType.JPEG);
 		user.addMedia(userMedia);
 
+		try{
+			img = ImageIO.read(new File("cat.jpg"));
+		}catch( IOException e){
+			
+		}
+		raster = img.getRaster();
+		data = (DataBufferByte) raster.getDataBuffer();
+		
 		Media userMedia2 = new Media();
-		addMedia(userMedia2);
+		addMedia(userMedia2, data.getData());
 		userMedia2.setCaption("me and my cattt");
 		userMedia2.setOwnerId(user.getUserId());
 		userMedia2.setMediaLocation("/media/" + userMedia2.getId() + "/"
