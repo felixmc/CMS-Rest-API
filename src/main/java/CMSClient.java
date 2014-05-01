6cb393abed58b3e5b5a69712fcab9c4340b19986
Felix Milea-Ciobanu <felixmilea@gmail.com>
2014-05-01 10:33:35 -0600

    added client file upload

--- src/main/java/CMSClient.java
+++ src/main/java/CMSClient.java
@@ -1,17 +1,23 @@
-
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
+import javax.imageio.ImageIO;
 import javax.ws.rs.core.Response;
 
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.apache.cxf.jaxrs.provider.json.JSONProvider;
 
-import edu.neumont.csc380.cms.model.Media;
 import edu.neumont.csc380.cms.service.MediaService;
 
-public class CMSClient{
-	private MediaService service = JAXRSClientFactory.create("http://localhost:8080/CMS-server/api", MediaService.class, Arrays.asList(new JSONProvider<Object>()));
-	
+public class CMSClient {
+	private MediaService service = JAXRSClientFactory.create(
+			"http://localhost:8080/CMS-server/api", MediaService.class,
+			Arrays.asList(new JSONProvider<Object>()));
+
 	public Response getMedia(Long id) {
 		return service.getMedia(id);
 	}
@@ -28,19 +34,21 @@ public class CMSClient{
 		service.deleteMedia(id);
 	}
 
-	public Response getUserMedia(Long id){
+	public Response getUserMedia(Long id) {
 		return service.getMediaByUser(id);
 	}
-	
-	public Response getAuctionMedia(Long id){
+
+	public Response getAuctionMedia(Long id) {
 		return service.getMediaByAuction(id);
 	}
-	
-	public void setUserProfilePicture(Long id, String caption, byte[] data, String mimeType){
+
+	public void setUserProfilePicture(Long id, String caption, byte[] data,
+			String mimeType) {
 		service.setUserProfilePicture(id, caption, data, mimeType);
 	}
-	
-	public void addAuctionMedia(Long id, String caption, byte[] data, String mimeType){
+
+	public void addAuctionMedia(Long id, String caption, byte[] data,
+			String mimeType) {
 		service.addAuctionMedia(id, caption, data, mimeType);
 	}
 
@@ -48,5 +56,36 @@ public class CMSClient{
 		CMSClient c = new CMSClient();
 		System.out.println(c.getAuctionMedia(2L).getEntity().toString());
 		System.out.println(c.getUserMedia(1L));
+
+		testAuctionImageUpload(c);
+		testUserImageUpload(c);
+	}
+
+	public static void testAuctionImageUpload(CMSClient c) {
+		File imgPath = new File("image.jpg");
+		BufferedImage bufferedImage;
+		try {
+			bufferedImage = ImageIO.read(imgPath);
+			WritableRaster raster = bufferedImage.getRaster();
+			DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
+			c.addAuctionMedia(2L, "lorem ipsum dolor sit amet", data.getData(),
+					"image/jpeg");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void testUserImageUpload(CMSClient c) {
+		File imgPath = new File("image.jpg");
+		BufferedImage bufferedImage;
+		try {
+			bufferedImage = ImageIO.read(imgPath);
+			WritableRaster raster = bufferedImage.getRaster();
+			DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
+			c.addAuctionMedia(1L, "lorem ipsum dolor sit amet", data.getData(),
+					"image/jpeg");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
